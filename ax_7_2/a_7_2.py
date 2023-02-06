8d840e4e58255d285f94eaeeb71412d4ac493cfa
Natalia Torres <torresloranatalia@gmail.com>
2023-02-06 14:19:01 -0500

    Completed assignment

---
+++ ax_7_2/a_7_2.py
@@ -0,0 +1,37 @@
+# 7.2 Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
+# X-DSPAM-Confidence:    0.8475
+# Count these lines and extract the floating point values from each of the lines and compute the average of those values and produce an output as shown below. Do not use the sum() function or a variable named sum in your solution.
+# You can download the sample data at http://www.py4e.com/code3/mbox-short.txt when you are testing below enter mbox-short.txt as the file name.
+
+# Use the file name mbox-short.txt as the file name
+# fname = input("Enter file name: ")
+
+# try: 
+#     with open(fname, "r") as fh:
+#         count = 0 
+#         sum = 0
+#         for line in fh:
+#             if not line.startswith("X-DSPAM-Confidence:"):
+#                 continue
+#             count+=1
+#             num = float(line[line.find(":")].strip())
+#             sum += num
+#         average = sum/count
+#         print("Average spam confidance:", average)
+# except FileNotFoundError:
+#     print("Error: the file could not be found.")
+
+fname = input("Enter file name: ")
+
+with open(fname, "r") as fh:
+    count = 0 
+    total = 0
+    for line in fh:
+        if not line.startswith("X-DSPAM-Confidence:"):
+            continue
+        count+=1
+        num = float(line.split(":")[1].strip())
+        total += num
+    average = total/count
+    print("Average spam confidence:", average)
+
